9d4557cc85deea9e6d8be7016613b427285180fe
hyomin <miamiq0000@gmail.com>
2025-09-30 13:06:32 +0900

    chore #10 소셜 로그인 onClick 함수 추가

--- src/components/social-login/social-login-list.tsx
+++ src/components/social-login/social-login-list.tsx
@@ -13,17 +13,20 @@ export default function SocialLoginList() {
       name: "카카오로 시작하기",
       icon: Kakao,
       className: "bg-[#FEE500] text-grayscale-black! hover:bg-[#FEE500]/80",
+      onClick: () => {},
     },
     {
       name: "네이버로 시작하기",
       icon: Naver,
       className: "bg-[#03C75A] text-grayscale-white! hover:bg-[#03C75A]/80",
+      onClick: () => {},
     },
     {
       name: "구글로 시작하기",
       icon: Google,
       className:
         "bg-tertiary-500 text-grayscale-black! hover:bg-tertiary-500/80",
+      onClick: () => {},
     },
 
     {
@@ -31,12 +34,13 @@ export default function SocialLoginList() {
       icon: Apple,
       className:
         "bg-grayscale-black text-grayscale-white! hover:bg-grayscale-black/80",
+      onClick: () => {},
     },
   ];
   return (
     <div className="space-y-3 w-full col-span-4 col-start-2">
-      {socialLoginInfo.map(({ name, icon: Icon, className }) => (
-        <SocialLoginButton key={name} className={className}>
+      {socialLoginInfo.map(({ name, icon: Icon, className, onClick }) => (
+        <SocialLoginButton key={name} className={className} onClick={onClick}>
           <SocialLoginIcon className="size-4">
             <Icon />
           </SocialLoginIcon>
