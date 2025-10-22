669dee14ce12ccbe23b31d01db0586813d4b8219
heeyoung123 <heeyoung321@naver.com>
2025-10-22 18:36:03 +0900

    refactor(#13): SVG 아이콘 시스템 개선

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,21 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
