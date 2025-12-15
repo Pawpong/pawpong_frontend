69938af339186b27a2c7d58fe8643039dd408769
heeyoung123 <heeyoung321@naver.com>
2025-12-15 16:55:41 +0900

    fix : explore 진입 시 강아지 탭 기본 설정

--- src/hooks/use-animal.tsx
+++ src/hooks/use-animal.tsx
@@ -4,7 +4,7 @@ import { useSegment } from './use-segment';
 
 export default function useAnimal(): 'cat' | 'dog' {
   const setAnimal = useFilterStore((state) => state.setAnimal);
-  const animal = (useSegment(1) as 'cat' | 'dog') || 'cat';
+  const animal = (useSegment(1) as 'cat' | 'dog') || 'dog';
   const clearActiveFilters = useFilterStore((state) => state.clearActiveFilters);
   // animal 값이 변경될 때마다 스토어의 상태를 업데이트합니다.
   useEffect(() => {
