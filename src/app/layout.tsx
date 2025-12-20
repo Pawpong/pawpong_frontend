e8b5cd22bd63797b87d65b7521ab04ee36663d6c
heeyoung123 <heeyoung321@naver.com>
2025-12-20 17:50:29 +0900

    feat : 메타태그 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,11 @@ export const metadata: Metadata = {
   title: '포퐁(Pawpong) | 반려동물 브리더와 입양자를 연결해요',
   description:
     '믿을 수 있는 전문 브리더의 강아지·고양이를 한 곳에서 만나보세요.\n브리더 정보부터 입양 가능한 아이, 상담과 후기까지 투명하게 확인할 수 있어요.',
+  verification: {
+    other: {
+      'naver-site-verification': '6a2ee16f52ebf338c18bc6b7c5a9a7b1b394e3e0',
+    },
+  },
 };
 
 export default function RootLayout({
