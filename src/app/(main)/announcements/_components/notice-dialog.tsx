10ca528145458083c95e74c3b4dba5ca6ebef3d5
heeyoung123 <heeyoung321@naver.com>
2025-12-21 14:06:27 +0900

    feat : (공지사항) 오버플로우 추가

--- src/app/(main)/announcements/_components/notice-dialog.tsx
+++ src/app/(main)/announcements/_components/notice-dialog.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import {
   LargeDialog,
@@ -7,11 +7,11 @@ import {
   LargeDialogHeader,
   LargeDialogTitle,
   LargeDialogTrigger,
-} from "@/components/ui/large-dialog";
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
-import Close from "@/assets/icons/close";
-import { NoticeItem } from "../_hooks/use-notices";
+} from '@/components/ui/large-dialog';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import Close from '@/assets/icons/close';
+import { NoticeItem } from '../_hooks/use-notices';
 
 interface NoticeDialogProps {
   notice: NoticeItem;
@@ -22,13 +22,11 @@ export default function NoticeDialog({ notice, children }: NoticeDialogProps) {
   return (
     <LargeDialog>
       <LargeDialogTrigger asChild>{children}</LargeDialogTrigger>
-      <LargeDialogContent className="w-full md:w-[600px] h-full md:h-[600px] md:max-w-[600px] md:max-h-[600px]">
-        <LargeDialogHeader className="pt-6 px-6 pb-[10px]">
+      <LargeDialogContent className="w-full md:w-[600px] h-full md:h-[600px] md:max-w-[600px] md:max-h-[600px] flex flex-col overflow-hidden">
+        <LargeDialogHeader className="pt-6 px-6 pb-[10px] shrink-0">
           <LargeDialogTitle>
             <div className="flex justify-between items-center w-full">
-              <span className="text-body-l font-semibold text-primary">
-                공지사항
-              </span>
+              <span className="text-body-l font-semibold text-primary">공지사항</span>
               <LargeDialogClose asChild>
                 <Button variant="secondary" size="icon" className="size-9">
                   <Close className="size-5 text-grayscale-gray7" />
@@ -39,23 +37,17 @@ export default function NoticeDialog({ notice, children }: NoticeDialogProps) {
         </LargeDialogHeader>
 
         {/* 스크롤 가능한 콘텐츠 영역 */}
-        <div className="flex flex-col gap-4 px-6 py-5 pb-10 flex-1 min-h-0 ">
+        <div className="flex flex-col gap-4 px-6 py-5 pb-10 flex-1 min-h-0 overflow-y-auto">
           {/* 공지사항 제목과 날짜 */}
           <div className="flex flex-col gap-2">
-            <p className="text-body-m font-medium text-primary">
-              {notice.title}
-            </p>
-            <p className="text-body-s font-normal text-grayscale-gray5">
-              {notice.date}
-            </p>
+            <p className="text-body-m font-medium text-primary">{notice.title}</p>
+            <p className="text-body-s font-normal text-grayscale-gray5">{notice.date}</p>
           </div>
 
           <Separator />
 
           {/* 본문 내용 */}
-          <p className="text-body-s font-normal text-grayscale-gray5 whitespace-pre-line">
-            {notice.content}
-          </p>
+          <p className="text-body-s font-normal text-grayscale-gray5 whitespace-pre-line">{notice.content}</p>
         </div>
       </LargeDialogContent>
     </LargeDialog>
