7e25669813521bfd5d73e1ad55ad29c3ece63136
rtaeho <ryou0920@naver.com>
2025-12-22 23:15:05 +0900

    fix: 햄버거 상태에서 로고 클릭 시 홈으로 이동하게 수정

--- src/components/gnb/mobile-nav-header.tsx
+++ src/components/gnb/mobile-nav-header.tsx
@@ -1,4 +1,8 @@
-import LogoButton from './logo-button';
+'use client';
+
+import Logo from '@/assets/logo/logo';
+import { Button } from '../ui/button';
+import Link from 'next/link';
 import NoticeButton from './notice-button';
 import Close from '@/assets/icons/close';
 import { SheetClose } from '../ui/sheet';
@@ -6,7 +10,13 @@ import { SheetClose } from '../ui/sheet';
 export default function MobileNavHeader() {
   return (
     <div className="flex items-center justify-between h-16 px-5 pr-[18px] py-3 shrink-0">
-      <LogoButton />
+      <SheetClose asChild>
+        <Link href="/">
+          <Button variant={'ghost'} className="py-2 -mx-3 -my-2 h-auto has-[>svg]:px-2">
+            <Logo className="w-20 h-auto text-primary" />
+          </Button>
+        </Link>
+      </SheetClose>
       <div className="flex gap-4 items-center">
         <NoticeButton />
         <SheetClose asChild>
