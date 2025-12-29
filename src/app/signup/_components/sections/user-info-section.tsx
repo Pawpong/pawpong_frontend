5beb3d2aab100f1aae54c0f2545a10144ef2245e
heeyoung123 <heeyoung321@naver.com>
2025-12-30 01:43:41 +0900

    fix : 모바일 GNB 로그인 버튼 개선

--- src/app/signup/_components/sections/user-info-section.tsx
+++ src/app/signup/_components/sections/user-info-section.tsx
@@ -392,7 +392,7 @@ export default function UserInfoSection() {
             </div>
           )}
         </div>
-        <div className="flex flex-col gap-2.5">
+        <div className="flex flex-col gap-2">
           <div className="py-2 flex items-center">
             <Label className="flex-1">
               <Checkbox checked={age14Checked} onCheckedChange={setAge14Checked} />
