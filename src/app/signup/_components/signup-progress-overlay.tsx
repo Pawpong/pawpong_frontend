9163839302ef2432f7caa60a0c30258f0dc9cf45
heeyoung123 <heeyoung321@naver.com>
2025-12-30 04:37:02 +0900

    design : (브리더 회원가입) 왼쪽 배너 디자인 수정

--- src/app/signup/_components/signup-progress-overlay.tsx
+++ src/app/signup/_components/signup-progress-overlay.tsx
@@ -35,17 +35,19 @@ export default function SignupProgressOverlay() {
       {/* 컨텐츠 */}
       <div className="relative text-center space-y-8 max-w-md w-full h-full flex flex-col justify-center py-8">
         {/* 타이틀 */}
-        <div className="space-y-4 mb-12">
-          <h1 className="text-[38px] leading-[1.2] font-bold text-white">
+        <div className="space-y-4 mb-[4.13rem]">
+          <h1 className="text-center text-tertiary-500 font-['NanumSquareOTF'] text-[1.75rem] font-extrabold leading-[140%]">
             믿을 수 있는 브리더분들을
             <br />
             기다리고 있어요.
           </h1>
-          <p className="text-[20px] leading-[1.4] font-medium text-white">당신의 브리딩 스토리를 세상에 알려주세요.</p>
+          <p className="text-center text-[var(--tertiary-500-basic,#F6F6EA)]  text-body-m font-medium">
+            당신의 브리딩 스토리를 세상에 알려주세요.
+          </p>
         </div>
 
         {/* 진행 단계 카드들 */}
-        <div className="space-y-2.5 w-full flex flex-col items-center">
+        <div className="space-y-2.5  flex flex-col items-center ">
           {steps.map((step) => {
             // flowIndex 0일 때는 첫 번째 단계를 활성으로 표시
             const currentStepIndex = flowIndex === 0 ? 1 : flowIndex;
@@ -55,20 +57,21 @@ export default function SignupProgressOverlay() {
               <div
                 key={step.number}
                 className={`
-                  flex items-center gap-4 px-5 py-3.5 rounded-2xl max-w-sm w-full
-                  transition-all duration-300 
+                  flex items-center h-[3.4675rem] px-[1.18044rem] gap-[0.81156rem] w-[20rem]
+                  rounded-lg transition-all duration-300  
                   ${isActive ? 'bg-secondary-500 shadow-xl' : 'bg-tertiary-500 shadow-md'}
                 `}
               >
                 <div
                   className={`
-                  flex items-center justify-center w-9 h-9 rounded-full text-[15px] font-bold shrink-0 bg-primary-500
-                  ${isActive ? 'text-secondary-500' : ' text-white'}
+                  flex items-center justify-center w-5 h-5 rounded-full shrink-0 bg-primary-500
+                  font-['Pretendard'] text-[11.8px] font-bold leading-[160%] tracking-[-0.5%] align-middle
+                  ${isActive ? 'text-secondary-500' : 'text-white'}
                 `}
                 >
                   {step.number}
                 </div>
-                <span className={`text-[17px] font-bold text-primary-500`}>{step.label}</span>
+                <span className={`text-[14px] font-bold text-primary-500 leading-[160%]`}>{step.label}</span>
               </div>
             );
           })}
